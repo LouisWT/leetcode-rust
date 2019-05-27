06e9807077088864447af8f12c156971e6168b78
LouisWT <louiswt@foxmail.com>
2019-05-27 17:18:10 +0800

    81题：在旋转数组中使用折半查找来查找target

--- src/n0081_search_in_rotated_sorted_array_ii.rs
+++ src/n0081_search_in_rotated_sorted_array_ii.rs
@@ -34,7 +34,37 @@ pub struct Solution {}
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> bool {
-        false
+    if nums.len() == 0 {
+        return false;
+    }
+    let mut left = 0;
+    let mut right = nums.len() - 1;
+    while left <= right {
+        let mut mid = left + (right - left) / 2;
+        if nums[mid] == target {
+        return true;
+        }
+        // 左边有序了
+        if nums[left] < nums[mid] {
+        // 如果 target 比左边最小的值还小
+        // 或者 target 比左边序列目前的最大值还大
+        // left = mid + 1;
+        if (nums[left] <= target) && (nums[mid] >= target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+        } else if nums[left] > nums[mid] {
+        if (target >= nums[mid]) && (target <= nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        } else {
+        left += 1;
+        }
+    }
+    return false;
     }
 }
 
@@ -46,5 +76,7 @@ mod tests {
 
     #[test]
     fn test_81() {
+        assert_eq!(Solution::search(vec![2,5,6,0,0,1,2], 0), true);
+        assert_eq!(Solution::search(vec![2,5,6,0,0,1,2], 3), false);
     }
 }
